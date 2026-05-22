e420e8645d228ca710c5e1487b044fcd0bcfd959
BapttX <bapt597@gmail.com>
2026-05-22 20:02:18 +0200

    Ajout des modèles + MFS + Migrations associations n-n

---
+++ database/factories/MusiqueFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Musique;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Musique>
+ */
+class MusiqueFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
