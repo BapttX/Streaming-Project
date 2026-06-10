5d696d6ef055c27c48067a1cedf219ad9337a4e3
BapttX <bapt597@gmail.com>
2026-06-10 02:24:28 +0200

    Correction Playlist fillable

--- app/Models/Playlist.php
+++ app/Models/Playlist.php
@@ -10,7 +10,7 @@ class Playlist extends Model
     /** @use HasFactory<\Database\Factories\PlaylistFactory> */
     use HasFactory;
 
-    protected $fillable = ['nom', 'user_id'];
+    protected $fillable = ['nomPlaylist', 'user_id'];
     
     /**
      * La playlist appartient à un utilisateur
